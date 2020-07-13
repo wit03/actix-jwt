eb7d148f19bb1c1ed3600d1aaf2bdc548a328028
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-13 21:54:40 +0700

    :sparkles: add db connection and .env file

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,18 @@ pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok(); //get value from .env file
     std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    //db connection
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    //start server
     HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
