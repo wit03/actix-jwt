062f9c7faf1084f83dbb0706517c091149ef0f49
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-07-14 00:11:05 +0700

    :sparkles: add db's model

---
+++ src/models.rs
@@ -0,0 +1,23 @@
+use crate::schema::*;
+use serde::{ Deserialize, Serialize };
+use diesel::sql_types::Timestamp;
+
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+pub struct Users {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub created_at: chrono::NaiveDateTime
+}
+
+//struct for auto generation of id
+#[derive(Insertable, Debug)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub first_name: &'a str,
+    pub last_name: &'a str,
+    pub email: &'a str,
+    pub created_at: chrono::NaiveDateTime
+}
+
